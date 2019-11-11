42d5eb0df3a62cc9669b838dff59711f883db72a
Natalie Galligan <nat.galligan@gmail.com>
2019-11-11 01:12:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,19 +18,11 @@ end
 # using director_data as input
 def gross_for_director(director_data)
 
-# what is exactly director data?
-# What will be the output?
-
-# To get all :worldwide_grosses from a particular director, we need to loop through their movies.
-# How to locate (long way):
-# nds[director_data?][:movies][counter][:worldwide_gross]
-# don't forget counter!
-
 counter = 0
 moneygross_for_director = 0
 
 while counter < director_data[:movies].count do
-  moneygross_for_director =+ director_data[:movies][counter][:worldwide_gross]
+  moneygross_for_director += director_data[:movies][counter][:worldwide_gross]
   counter += 1
 end
 
