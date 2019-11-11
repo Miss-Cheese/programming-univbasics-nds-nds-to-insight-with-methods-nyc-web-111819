e770b67b1cf6d88712af73d74cbbb0a78185fab2
Natalie Galligan <nat.galligan@gmail.com>
2019-11-11 00:59:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,5 +17,17 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  # puts director_data
+
+# what is exactly director data?
+# What will be the output?
+
+# To get all :worldwide_grosses from a particular director, we need to loop through their movies.
+# How to locate (long way):
+# nds[director_data?][:movies][counter][:worldwide_gross]
+# don't forget counter!
+
+  puts director_data
+
 end
+
+gross_for_director(Robert Zemeckis)
